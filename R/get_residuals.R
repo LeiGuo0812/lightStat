fb68c2f2c165c872883098f7d4873d6d43f5ac32
LeiGuo0812 <guolei0812@yeah.net>
2021-02-27 16:32:04 +0800

    add new function

---
+++ R/get_residuals.R
@@ -0,0 +1,33 @@
+#' get_residuals
+#'
+#' @param data a data frame or tibble, contains the columns to calculate residuals
+#' @param cov a data frame or tibble, contains the columns as covariate. The length of row should be the same as data
+#' @param addmean logical, should the mean of original data should be added back to the residual, default is true
+#'
+#' @return a data frame with same dimension with data, containing the residuals of the original columns
+#' @export
+#'
+#' @examples
+#' data("mtcars")
+#' get_residuals(mtcars[1:3], mtcars[4:5])
+#'
+#' #get residual without adding mean back
+#' get_residuals(mtcars[1:3], mtcars[4:5],addmean = FALSE)
+#'
+get_residuals <- function(data,cov,addmean = TRUE){
+  lm_data <- cbind(data,cov)
+  var <- colnames(data)
+  cov <- colnames(cov)
+  results <- lapply(var, function(x){
+    formula <- as.formula(paste(x,paste(cov,collapse = '+'),sep = '~'))
+    model <- lm(formula = formula, data = lm_data)
+    if (addmean == TRUE) {
+      return(model$residuals + mean(dplyr::pull(model$model[x]),na.rm = T))
+    } else {
+      return(model$residuals)
+    }
+  })
+  names(results) <- paste0(var,'_residual')
+  results %>% as.data.frame() -> results
+  return(results)
+}
