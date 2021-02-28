ebb1656462e492d54d6dc2e1c206fd090151fe95
LeiGuo0812 <guolei0812@yeah.net>
2021-02-28 14:33:32 +0800

    sovled dependency problems

--- R/corr_proper.R
+++ R/corr_proper.R
@@ -1,5 +1,11 @@
 #' corr_proper
 #' @description chooses proper correlation method (pearson or spearman) based on normality of the tested variables.
+#' @importFrom magrittr %>%
+#' @importFrom dplyr pull mutate select across all_of filter rename left_join everything
+#' @importFrom tidyr pivot_longer
+#' @importFrom tibble as_tibble
+#' @importFrom psych corr.test
+#' @importFrom stats shapiro.test
 #' @param data  a dataframe or tibble.
 #' @param y the second matrix or dataframe with the same number of rows as data.If y is provided, the correlation just between variables in data and y will be calculated. If y is not provided, pair-wise correlation between variables in data will be calculated.
 #' @param p.adjust p.adjust receive a string of holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr", "none", which would conduct p value adjust.
@@ -38,34 +44,34 @@ corr_proper <- function(data, y = NULL, p.adjust = NULL){
     #Loop for paire-wise variable
     for (i in 1:length(data)) {
       for (j in 1:length(data)) {
-        if(is.character(data[i] %>% dplyr::pull()) |
-           is.character(data[j] %>% dplyr::pull())){
+        if(is.character(data[i] %>% pull()) |
+           is.character(data[j] %>% pull())){
           cor <- list(r = NA,
                       p = NA,
                       n = NA,
                       t = NA,
                       se = NA)
           results$method[i,colnames(data)[j]] <- NA
-        } else if (is.ordered(data[i] %>% dplyr::pull()) &
-                   is.ordered(data[j] %>% dplyr::pull())) {
-          cor <- psych::corr.test(as.numeric(data[i] %>% pull()),
+        } else if (is.ordered(data[i] %>% pull()) &
+                   is.ordered(data[j] %>% pull())) {
+          cor <- corr.test(as.numeric(data[i] %>% pull()),
                                   as.numeric(data[j] %>% pull()),
                                   method = 'kendall')
           #Record test method
           results$method[i,colnames(data)[j]] <- 'kendall'
         }
         else if(
-          (is.ordered(data[i] %>% dplyr::pull()) &
-           !is.factor(data[j] %>% dplyr::pull())) |
-          (!is.factor(data[i] %>% dplyr::pull()) &
-           is.ordered(data[j] %>% dplyr::pull()))){
-          cor <- psych::corr.test(as.numeric(data[i] %>% pull()),
+          (is.ordered(data[i] %>% pull()) &
+           !is.factor(data[j] %>% pull())) |
+          (!is.factor(data[i] %>% pull()) &
+           is.ordered(data[j] %>% pull()))){
+          cor <- corr.test(as.numeric(data[i] %>% pull()),
                                   as.numeric(data[j] %>% pull()),
                                   method = 'spearman')
           #Record test method
           results$method[i,colnames(data)[j]] <- 'spearman'
-        } else if (is.factor(data[i] %>% dplyr::pull()) |
-                   is.factor(data[j] %>% dplyr::pull())) {
+        } else if (is.factor(data[i] %>% pull()) |
+                   is.factor(data[j] %>% pull())) {
           cor <- list(r = NA,
                       p = NA,
                       n = NA,
@@ -79,14 +85,14 @@ corr_proper <- function(data, y = NULL, p.adjust = NULL){
           # If either variable does not meet normality
           if (normal[[1]]$p.value < 0.05 | normal[[2]]$p.value < 0.05 ) {
             #Corr test with spearman correlation
-            cor <- psych::corr.test(as.numeric(data[i] %>% pull()),
+            cor <- corr.test(as.numeric(data[i] %>% pull()),
                                     as.numeric(data[j] %>% pull()),
                                     method = 'spearman')
             #Record test method
             results$method[i,colnames(data)[j]] <- 'spearman'
           } else {
             #If both variables meet nomorlity, use pearson correlation
-            cor <- psych::corr.test(as.numeric(data[i] %>% pull()),
+            cor <- corr.test(as.numeric(data[i] %>% pull()),
                                     as.numeric(data[j] %>% pull()))
             results$method[i,colnames(data)[j]] <- 'pearson'
           }
@@ -113,34 +119,34 @@ corr_proper <- function(data, y = NULL, p.adjust = NULL){
     #Loop for paire-wise variable
     for (i in 1:length(data)) {
       for (j in 1:length(y)) {
-        if(is.character(data[i] %>% dplyr::pull()) |
-           is.character(y[j] %>% dplyr::pull())){
+        if(is.character(data[i] %>% pull()) |
+           is.character(y[j] %>% pull())){
           cor <- list(r = NA,
                       p = NA,
                       n = NA,
                       t = NA,
                       se = NA)
           results$method[i,colnames(y)[j]] <- NA
-        } else if (is.ordered(data[i] %>% dplyr::pull()) &
-                   is.ordered(y[j] %>% dplyr::pull())) {
-          cor <- psych::corr.test(as.numeric(data[i] %>% pull()),
+        } else if (is.ordered(data[i] %>% pull()) &
+                   is.ordered(y[j] %>% pull())) {
+          cor <- corr.test(as.numeric(data[i] %>% pull()),
                                   as.numeric(y[j] %>% pull()),
                                   method = 'kendall')
           #Record test method
           results$method[i,colnames(y)[j]] <- 'kendall'
         }
         else if(
-          (is.ordered(data[i] %>% dplyr::pull()) &
-           !is.factor(y[j] %>% dplyr::pull())) |
-          (!is.factor(data[i] %>% dplyr::pull()) &
-           is.ordered(y[j] %>% dplyr::pull()))){
-          cor <- psych::corr.test(as.numeric(data[i] %>% pull()),
+          (is.ordered(data[i] %>% pull()) &
+           !is.factor(y[j] %>% pull())) |
+          (!is.factor(data[i] %>% pull()) &
+           is.ordered(y[j] %>% pull()))){
+          cor <- corr.test(as.numeric(data[i] %>% pull()),
                                   as.numeric(y[j] %>% pull()),
                                   method = 'spearman')
           #Record test method
           results$method[i,colnames(y)[j]] <- 'spearman'
-        } else if (is.factor(data[i] %>% dplyr::pull()) |
-                   is.factor(y[j] %>% dplyr::pull())) {
+        } else if (is.factor(data[i] %>% pull()) |
+                   is.factor(y[j] %>% pull())) {
           cor <- list(r = NA,
                       p = NA,
                       n = NA,
@@ -154,14 +160,14 @@ corr_proper <- function(data, y = NULL, p.adjust = NULL){
           # If either variable does not meet normality
           if (normal[[1]]$p.value < 0.05 | normal[[2]]$p.value < 0.05 ) {
             #Corr test with spearman correlation
-            cor <- psych::corr.test(as.numeric(data[i] %>% pull()),
+            cor <- corr.test(as.numeric(data[i] %>% pull()),
                                     as.numeric(y[j] %>% pull()),
                                     method = 'spearman')
             #Record test method
             results$method[i,colnames(y)[j]] <- 'spearman'
           } else {
             #If both variables meet nomorlity, use pearson correlation
-            cor <- psych::corr.test(as.numeric(data[i] %>% pull()),
+            cor <- corr.test(as.numeric(data[i] %>% pull()),
                                     as.numeric(y[j] %>% pull()))
             results$method[i,colnames(y)[j]] <- 'pearson'
           }
@@ -179,27 +185,27 @@ corr_proper <- function(data, y = NULL, p.adjust = NULL){
   #Define a function to convert long
   cor_long <- function(x, value){
     x %>%
-      dplyr::mutate(rownames = rownames(.)) %>%
-      tibble::as_tibble() %>%
-      dplyr::select(rownames,everything()) %>%
-      tidyr::pivot_longer(-1, names_to = 'var2', values_to = value) %>%
-      dplyr::rename(var1 = rownames)
+      mutate(rownames = rownames(.)) %>%
+      as_tibble() %>%
+      select(rownames,everything()) %>%
+      pivot_longer(-1, names_to = 'var2', values_to = value) %>%
+      rename(var1 = rownames)
   }
   #Combine corr test results in long format
   results[['cor_long']] <- suppressMessages(cor_long(results$r,'r') %>%
-                                              dplyr::filter(var1 != var2) %>%
-                                              dplyr::filter(!duplicated(r)) %>%
-                                              dplyr::filter(!is.na(r)) %>%
-                                              dplyr::left_join(cor_long(results$p,'p')) %>%
-                                              dplyr::left_join(cor_long(results$n,'n')) %>%
-                                              dplyr::left_join(cor_long(results$method,'method')) %>%
-                                              dplyr::left_join(cor_long(results$t,'t')) %>%
-                                              dplyr::left_join(cor_long(results$se,'se')))
+                                              filter(var1 != var2) %>%
+                                              filter(!duplicated(r)) %>%
+                                              filter(!is.na(r)) %>%
+                                              left_join(cor_long(results$p,'p')) %>%
+                                              left_join(cor_long(results$n,'n')) %>%
+                                              left_join(cor_long(results$method,'method')) %>%
+                                              left_join(cor_long(results$t,'t')) %>%
+                                              left_join(cor_long(results$se,'se')))
 
   #Conduct p adjust if p.adjust is provided
   if (!is.null(p.adjust)) {
     results$cor_long <- results$cor_long %>%
-      dplyr::mutate(p.adj = p.adjust(p, method = p.adjust))
+      mutate(p.adj = p.adjust(p, method = p.adjust))
   }
   return(results)
 }
